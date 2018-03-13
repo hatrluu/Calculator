b26f8344ad2df2b6b23b4df4f964cc0ec2cb73ca
Hau Luu <hatrluu@gmail.com>
2018-03-13 16:24:11 -0400

    Completed all functions, redesign layout

--- app/src/main/java/com/example/h/calculator/MainActivity.java
+++ app/src/main/java/com/example/h/calculator/MainActivity.java
@@ -14,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
     float val2 = 0;
 
     float memory, result;
-    boolean mAddition = true;
-    boolean mSubtraction, mMultiplication, mDivision;
+    String optr = "+";
+    boolean isVal1Empty = true;
+    boolean newCalc = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +47,12 @@ public class MainActivity extends AppCompatActivity {
         memclear = findViewById(R.id.memclear);
         editText = findViewById(R.id.editText);
 
+        //Set up numpads
         num1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("1");
                 } else {
                     editText.setText(editText.getText() + "1");
@@ -61,165 +63,223 @@ public class MainActivity extends AppCompatActivity {
         num2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("2");
-                } else {editText.setText(editText.getText() + "2");}
+                } else {
+                    editText.setText(editText.getText() + "2");
+                }
             }
         });
         num3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("3");
-                } else {editText.setText(editText.getText() + "3");}
+                } else {
+                    editText.setText(editText.getText() + "3");
+                }
             }
         });
         num4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("4");
-                } else {editText.setText(editText.getText() + "4");}
+                } else {
+                    editText.setText(editText.getText() + "4");
+                }
             }
         });
         num5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("5");
-                } else {editText.setText(editText.getText() + "5");}
+                } else {
+                    editText.setText(editText.getText() + "5");
+                }
             }
         });
         num6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("6");
-                } else {editText.setText(editText.getText() + "6");}
+                } else {
+                    editText.setText(editText.getText() + "6");
+                }
             }
         });
         num7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("7");
-                } else {editText.setText(editText.getText() + "7");}
+                } else {
+                    editText.setText(editText.getText() + "7");
+                }
             }
         });
         num8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("9");
-                } else {editText.setText(editText.getText() + "8");}
+                } else {
+                    editText.setText(editText.getText() + "8");
+                }
             }
         });
         num9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (val1 == 0) {
-                    val1 = 1;
+                if (editText.getText().toString().equals("0") || newCalc == true) {
+                    newCalc = false;
                     editText.setText("9");
-                } else {editText.setText(editText.getText() + "9");}
+                } else {
+                    editText.setText(editText.getText() + "9");
+                }
             }
         });
         num0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!editText.getText().toString().equals("0")){
+                //Adding 0s
+                //When editText is not 0, e.g: 30, 40, etc.
+                //When there is a decimal point, e.g: 2.04, 0.005, etc.
+
+                //Set editText to 0
+                //When editText is 0
+                //After a calculation is made (new calculation)
+
+                if (!editText.getText().toString().equals("0") && newCalc == false) {
                     editText.setText(editText.getText() + "0");
+                } else if (editText.getText().toString().contains(".") && newCalc == false) {
+                    editText.setText(editText.getText() + "0");
+
+                } else if (newCalc == true) {
+                    newCalc = false;
+                    if(editText.getText().toString().contains(".")){
+                        editText.setText(editText.getText() + "0");
+                    }
+                    else{
+                        editText.setText("0");
+                    }
+
                 }
-                else{
-                    editText.setText("0");
+
+            }
+        });
+        period.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().contains(".")) {
+                    editText.setText(editText.getText() + ".");
                 }
             }
         });
 
+        //Set up operators
         addition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAddition = true;
-                if (val1 == 0) {
+                optr = "+";
+                if (isVal1Empty == true) {
                     val1 = Float.parseFloat(editText.getText() + "");
-                    mSubtraction = mMultiplication = mDivision = false;
                     editText.setText("");
-                }
-                else if (val2 != 0){
+                    isVal1Empty = false;
+                } else if (val2 != 0) {
                     val2 = 0;
                     editText.setText("");
-                }
-                else{
-                    val2 = Float.parseFloat(editText.getText()+"");
+                } else {
                     editText.setText("");
-                    val1 += val2;
-                    editText.setText(Float.toString(val1));
-
                 }
             }
         });
         subtraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                val1 = Float.parseFloat(editText.getText() + "");
-                mSubtraction = true;
-                mAddition = mMultiplication = mDivision = false;
-                editText.setText("");
+                optr = "-";
+                if (isVal1Empty == true) {
+                    val1 = Float.parseFloat(editText.getText() + "");
+                    editText.setText("");
+                    isVal1Empty = false;
+                } else if (val2 != 0) {
+                    val2 = 0;
+                    editText.setText("");
+                } else {
+                    editText.setText("");
+                }
             }
         });
         multiplication.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                val1 = Float.parseFloat(editText.getText() + "");
-                mMultiplication = true;
-                mSubtraction = mAddition = mDivision = false;
-                editText.setText("");
+                optr = "*";
+                if (isVal1Empty == true) {
+                    val1 = Float.parseFloat(editText.getText() + "");
+                    editText.setText("");
+                    isVal1Empty = false;
+                } else if (val2 != 0) {
+                    val2 = 0;
+                    editText.setText("");
+                } else {
+                    editText.setText("");
+                }
             }
         });
         division.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                val1 = Float.parseFloat(editText.getText() + "");
-                mDivision = true;
-                mSubtraction = mMultiplication = mAddition = false;
-                editText.setText("");
+                optr = "/";
+                if (isVal1Empty == true) {
+                    val1 = Float.parseFloat(editText.getText() + "");
+                    editText.setText("");
+                    isVal1Empty = false;
+                } else if (val2 != 0) {
+                    val2 = 0;
+                    editText.setText("");
+                } else {
+                    editText.setText("");
+                }
             }
         });
         equal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                val2 = Float.parseFloat(editText.getText() + "");
-                if (mAddition == true) {
-                    result = val1 + val2;
-                    editText.setText(result + "");
-                    mAddition = false;
-                    val1 = val2 = 0;
-                }
-                if (mSubtraction == true) {
-                    result = val1 - val2;
-                    editText.setText(result + "");
-                    mSubtraction = false;
-                    val1 = val2 = 0;
-                }
-
-                if (mMultiplication == true) {
-                    result = val1 * val2;
-                    editText.setText(result + "");
-                    mMultiplication = false;
-                    val1 = val2 = 0;
-                }
-
-                if (mDivision == true) {
-                    result = val1 / val2;
-                    editText.setText(result + "");
-                    mDivision = false;
-                    val1 = val2 = 0;
+                if (!editText.getText().toString().equals("")) {
+                    val2 = Float.parseFloat(editText.getText() + "");
+                    if (optr.equals("+")) {
+                        result = val1 + val2;
+                        editText.setText(result + "");
+                        val1 = 0;
+                        isVal1Empty = newCalc = true;
+                    }
+                    if (optr.equals("-")) {
+                        result = val1 - val2;
+                        editText.setText(result + "");
+                        val1 = 0;
+                        isVal1Empty = newCalc = true;
+                    }
+                    if (optr.equals("*")) {
+                        result = val1 * val2;
+                        editText.setText(result + "");
+                        val1 = 0;
+                        isVal1Empty = newCalc = true;
+                    }
+                    if (optr.equals("/")) {
+                        result = val1 / val2;
+                        editText.setText(result + "");
+                        val1 = 0;
+                        isVal1Empty = newCalc = true;
+                    }
                 }
             }
         });
@@ -229,6 +289,8 @@ public class MainActivity extends AppCompatActivity {
                 editText.setText("0");
             }
         });
+
+        //Memory calculations
         memadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
